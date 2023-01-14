2a13005d42c6b75daa7fb67787a476bafd597473
Mac_Developer <araozangel842@gmail.com>
2023-01-14 16:06:35 +0200

    Fetch images for bg and logo.
    Add styles

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,17 +1,26 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 import React from 'react';
 import CustomButton from '../components/Custom/CustomButton';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const imageUrlFromCloudinary =
     'https://res.cloudinary.com/dqaerysgb/image/upload/v1673656213/Random/main_bg_mab3uj.jpg';
+  const imageUrlFromCloudinaryFlutter =
+    'https://res.cloudinary.com/dqaerysgb/image/upload/v1673267854/Github/flutter-no-bg_rfrxng.png';
+  const goTo = () => {
+    navigation.navigate('MyCities');
+  };
   return (
     <View style={styles.container}>
       <ImageBackground source={{ uri: imageUrlFromCloudinary }} style={styles.imageBackground}>
-        <View style={styles.cnter}>
-          <Text style={{marginBottom: 100}}>Inside</Text>
-          <Text>HomeScreen</Text>
-            <CustomButton label="Create a new game"/>
+        <View style={styles.center}>
+          <Image
+            source={{ uri: imageUrlFromCloudinaryFlutter }}
+            style={styles.imageUrlFromCloudinaryFlutter}
+          />
+          <Text style={styles.middle}>Inside</Text>
+          <Text style={styles.weather}>{`Welcome to\n my weather station`}</Text>
+          <CustomButton label="Let's go..."radius={10} onPress={goTo} />
         </View>
       </ImageBackground>
     </View>
@@ -30,9 +39,30 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  cnter: {
+  center: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  imageUrlFromCloudinaryFlutter: {
+    width: 150,
+    height: 150,
+  },
+  middle: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'left',
+    marginLeft: 20,
+    textTransform: 'uppercase',
+  },
+  weather: {
+    color: '#fff',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginLeft: '20%',
+    textTransform: 'uppercase',
+    textAlign: 'center',
   },
 });
