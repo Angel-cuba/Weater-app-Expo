0da9cea4450e1c6233eb1fd890ed0d0e02896835
Mac_Developer <araozangel842@gmail.com>
2023-01-14 16:04:49 +0200

    Refactor code

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screen/HomeScreen';
-import MyCitiesScreen from './screen/MyCitiesScreen';
-import SearchScreen from './screen/SearchScreen';
-
-const Stack = createNativeStackNavigator();
+import Navigator from './navigation/Navigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="MyCities" component={MyCitiesScreen} />
-        <Stack.Screen name="Search" component={SearchScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigator/>
   );
 }
--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,12 +1,49 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from '../screen/HomeScreen';
+import MyCitiesScreen from '../screen/MyCitiesScreen';
+import SearchScreen from '../screen/SearchScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export default function Navigator() {
   return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="MyCities"
+          component={MyCitiesScreen}
+          options={{
+            title: 'My Cities',
+            headerStyle: {
+              backgroundColor: '#C1EEFB',
+            },
+            headerTintColor: '#000000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerBackTitleVisible: false,
+            headerShadowVisible: false,
+          }}
+        />
+        <Stack.Screen name="Search" component={SearchScreen} options={{
+          title: 'Search your city',
+          headerStyle: {
+            backgroundColor: '#C1EEFB',
+          },
+          headerTintColor: '#000000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerBackTitleVisible: false,
+          headerShadowVisible: false,
+          
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
