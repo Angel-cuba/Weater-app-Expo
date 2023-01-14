fd23338b88e3503be0a012fd6618cead98d058c1
Mac_Developer <araozangel842@gmail.com>
2023-01-15 01:56:37 +0200

    Refactor code

--- screen/MyCitiesScreen.js
+++ screen/MyCitiesScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, Pressable, Modal, Alert } from 'react-native';
 import React, { useState } from 'react';
 import CustomButton from '../components/Custom/CustomButton';
+import ModalScreen from '../components/Modal';
 
 const MyCitiesScreen = ({ navigation }) => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -23,36 +24,6 @@ const MyCitiesScreen = ({ navigation }) => {
 
 export default MyCitiesScreen;
 
-const ModalScreen = ({ modalVisible, setModalVisible, addCity }) => {
-  return (
-    <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <View style={styles.buttonPlusOnModal}>
-              <CustomButton
-                label="x"
-                radius={40}
-                onPress={() => setModalVisible(false)}
-                fontSize={20}
-              />
-            </View>
-          </View>
-        </View>
-      </Modal>
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -64,43 +35,5 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 70,
     right: 40,
-  },
-  buttonPlusOnModal: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-  },
-  // Modal
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.95)',
-  },
-  modalView: {
-    width: '90%',
-    height: '88%',
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
   },
 });
