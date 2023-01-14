6c379b87367fb15f42b2d5c8a50680db738d0709
Mac_Developer <araozangel842@gmail.com>
2023-01-14 16:07:04 +0200

    Ongoing work.
    I have added a modal for add cities! It is not yet functional, but it is there.

--- screen/SearchScreen.js
+++ screen/SearchScreen.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const SearchScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>SearchScreen</Text>
     </View>
   )
 }
 
 export default SearchScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C1EEFB',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
