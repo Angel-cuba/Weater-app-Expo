b41cad4d09d12afd8d6ad532451a2c07c07c86eb
Mac_Developer <araozangel842@gmail.com>
2023-01-15 16:41:28 +0200

    Add modal view and cities found.

--- components/City.js
+++ components/City.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import React from 'react';
 
-const City = () => {
+const City = ({ cities }) => {
   return (
-    <View>
-      <Text>City</Text>
-    </View>
-  )
-}
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.scroll}>
+      {cities.map((city) => {
+        return (
+          <View key={city.id} style={styles.cityItem}>
+            <Text style={styles.cityName}>{city.name}</Text>
+            <Text style={styles.cityCountry}>{city.country}</Text>
+            <Text style={styles.cityAdmin}>{city.adminArea}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
 
-export default City
+export default City;
+
+const styles = StyleSheet.create({
+  scroll: {
+    paddingHorizontal: 10,
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.0261)',
+    borderRadius: 10,
+  },
+  cityItem: {
+    backgroundColor: 'rgba(5,2,112,0.61)',
+    padding: 10,
+    margin: 3,
+    borderRadius: 10,
+  },
+  cityName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cityCountry: {
+    color: '#FFFFFF',
+    alignSelf: 'center',
+    fontSize: 15,
+  },
+  cityAdmin: {
+    fontSize: 15,
+    alignSelf: 'flex-end',
+  },
+
+
+  })
---
+++ components/Modal.js
@@ -0,0 +1,91 @@
+import { View, Text, StyleSheet, Modal } from 'react-native';
+import React from 'react';
+import CustomButton from './Custom/CustomButton';
+import CustomInput from './Custom/CustomInput';
+import { getData, token } from '../data/getCitiesData';
+import City from './City';
+
+const ModalScreen = ({ modalVisible, setModalVisible }) => {
+  const [search, setSearch] = React.useState('');
+  const [cities, setCities] = React.useState(null);
+
+  React.useEffect(() => {
+    const timeOut = setTimeout(
+      () =>
+        getData(search, token).then((data) => {
+          setCities(data.locations);
+        }),
+      1000
+    );
+    return () => clearTimeout(timeOut);
+  }, [search]);
+
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Write your city name</Text>
+            <CustomInput placeholder="Search" value={search} onChangeText={setSearch} />
+            {!cities || search === '' ? <Text>No cities selected</Text> : <City cities={cities} />}
+            <View style={styles.buttonPlusOnModal}>
+              <CustomButton
+                label="x"
+                radius={40}
+                onPress={() => setModalVisible(false)}
+                fontSize={20}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default ModalScreen;
+
+const styles = StyleSheet.create({
+  buttonPlusOnModal: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  // Modal
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.95)',
+  },
+  modalView: {
+    width: '90%',
+    height: '88%',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
